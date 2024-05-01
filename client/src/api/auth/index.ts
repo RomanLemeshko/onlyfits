668ee9926c23ef2dee64ae932e0251461f39260b
RomanLemeshko <romanlemeshcko@mail.ru>
2024-05-01 19:13:24 +0300

    Clear

--- client/src/api/auth/index.ts
+++
@@ -1,8 +0,0 @@
-
-import { AxiosPromise } from "axios";
-import Endpoints from "../endpoints";
-import { axiosInstance } from "../instance";
-import { LoginRequest, LoginResponse } from "./types";
-
-
-export const login = (params: LoginRequest): AxiosPromise<LoginResponse> => axiosInstance.post(Endpoints.AUTH.LOGIN, params)
--- client/src/api/endpoints.ts
+++
@@ -1,9 +0,0 @@
-const Endpoints = {
-    AUTH: {
-        LOGIN: '/login',
-        REFRESH: '/refresh',
-        LOGOUT: '/logout',
-    }
-}
-
-export default Endpoints
