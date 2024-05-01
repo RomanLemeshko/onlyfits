7db4b96c32d4ff61cecceb65233b4c79179c213e
Sergei Khvalev <khvalev.official@gmail.com>
2024-05-01 11:39:32 -0400

    Push main routing again

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
-
+//ref
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RouterProvider router ={router}/>
 )
