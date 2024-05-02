8cfb3a27e3a0c821528d822dcc3938169f4294c2
Sergei Khvalev <khvalev.official@gmail.com>
2024-05-02 13:50:36 -0400

    Seeders fixing

--- server/db/seeders/20240502004154-exercise_seed.js
+++ server/db/seeders/20240502004154-exercise_seed.js
@@ -1,25 +1,25 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
+
+function readJson() {
+  const folder = path.join(__dirname, '..', '..', 'exerciseData');
+  const file_name = 'exercise_data.json';
+  const data = fs.readFileSync(path.join(folder, file_name), 'utf-8');
+  const parsed = JSON.parse(data);
+  return parsed;
+}
+// readJson()
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const arrExercises = readJson();
+
+    await queryInterface.bulkInsert('Exercises', arrExercises, {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Exercises', null, {});
+  },
 };
