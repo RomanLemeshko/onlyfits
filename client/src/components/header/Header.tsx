66c299cbcf38d86377489f286b72e99f94df3705
RomanLemeshko <romanlemeshcko@mail.ru>
2024-05-20 19:35:41 +0300

    Fixed bag Header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -26,9 +26,11 @@ const Header = () => {
         {isLoggedIn ? (
           <>
           <div className='header-content'>
-            <div className="logo">
+          <Link to="/">
+            <div className="logo-header">
               <img src="https://firebasestorage.googleapis.com/v0/b/onlyfits-1ba90.appspot.com/o/onlyfits_logotype.png?alt=media&token=3e929633-b844-48cd-9158-1a8a2581c6de" alt="onlyfits_logo" />
             </div>
+          </Link>
             <div>
 
             <ul id="header-container" className="authenticated">
@@ -43,11 +45,21 @@ const Header = () => {
           </div>
           </>
         ) : (
-          <ul className="unauthenticated">
-            <li><Link to="/" className={location.pathname === "/" ? "active-link" : ""}>Home</Link></li>
-            <li><Link to="/register" className={location.pathname === "/register" ? "active-link" : ""}>Sign up</Link></li>
-            <li><Link to="/login" className={location.pathname === "/login" ? "active-link" : ""}>Sign in</Link></li>
-          </ul>
+          <>
+          <div className='header-content'>
+            <Link to="/">
+            <div className="logo-header">
+              <img src="https://firebasestorage.googleapis.com/v0/b/onlyfits-1ba90.appspot.com/o/onlyfits_logotype.png?alt=media&token=3e929633-b844-48cd-9158-1a8a2581c6de" alt="onlyfits_logo" />
+            </div>
+            </Link>
+            <ul className="unauthenticated">
+              <li><Link to="/" className={location.pathname === "/" ? "active-link" : ""}>Home</Link></li>
+              <li><Link to="/register" className={location.pathname === "/register" ? "active-link" : ""}>Sign up</Link></li>
+              <li><Link to="/login" className={location.pathname === "/login" ? "active-link" : ""}>Sign in</Link></li>
+            </ul>
+            <p>UserUserUserUserUserUser</p>
+          </div>
+          </>
         )}
       </div>
     </div>
