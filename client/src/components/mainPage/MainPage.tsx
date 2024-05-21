c3905df16d5bc7d55d781f74726ce577b5eb6915
IsmailTakhmanov <itakhmanov@gmail.com>
2024-05-21 17:45:03 +0300

    added animation for dropdown menu

--- client/src/components/mainPage/MainPage.tsx
+++ client/src/components/mainPage/MainPage.tsx
@@ -59,12 +59,7 @@ const MainPage = () => {
 
   const progFilterHandler = () => {
     if (programType !== 'All' || programLevel !== 'All') {
-      console.log(programs);
-      console.log('!!!!', { type: programType, level: programLevel });
       dispatch(setFilteredPrograms({ type: programType, level: programLevel }));
-
-      console.log('@#@#, LEVEL', programLevel);
-      console.log('@#@#, TYPE', programType);
     } else {
       dispatch(resetFilters());
     }
@@ -77,7 +72,6 @@ const MainPage = () => {
       )
         .unwrap()
         .then(() => {
-          console.log('Program added successfully');
           dispatch(getUserProgramsThunky(user.id));
         })
         .catch((error) => {
@@ -86,43 +80,13 @@ const MainPage = () => {
     }
   };
 
-  //! for dropdown filter properties
-
   return (
     <div>
       <Header />
       <div id="programs-filters-container">
-        {/* <div className="filter-container">
-          <label htmlFor="programType">Training type:</label>
-          <select
-            id="programType"
-            className="program-level-type-filter"
-            onChange={(e) => setProgramType(e.target.value)}
-          >
-            <option value="all">All</option>
-            <option value="cardio">Cardio</option>
-            <option value="strength">Strength</option>
-            <option value="stretching">Stretching</option>
-          </select>
-        </div>
-        <div className="filter-container">
-          <label htmlFor="programLevel">Difficulty level:</label>
-          <select
-            id="programLevel"
-            className="program-level-type-filter"
-            onChange={(e) => setProgramLevel(e.target.value)}
-          >
-            <option value="all">All</option>
-            <option value="beginner">Beginner</option>
-            <option value="medium">Medium</option>
-            <option value="professional">Professional</option>
-          </select>
-        </div> */}
-
         <DropdownMenu
           styleName="type"
           listOfOptions={listOfOptionsType}
-          label="TRAIDING TYPE: "
           initialMenuMessage="all"
           setProgramFilter={setProgramType}
         />
@@ -130,7 +94,6 @@ const MainPage = () => {
         <DropdownMenu
           styleName="level"
           listOfOptions={listOfOptionsLevel}
-          label="DIFFICULTY LEVEL: "
           initialMenuMessage="all"
           setProgramFilter={setProgramLevel}
         />
@@ -142,11 +105,10 @@ const MainPage = () => {
       <div id="general">
         <div className="programs-container">
           {programs.map((eachProgram: ProgramType) => (
-            <div className="card-container">
+            <div className="card-container" key={eachProgram.id}>
               <Card
                 className="card"
                 hoverable
-                key={eachProgram.id}
                 size="small"
                 cover={
                   <img
@@ -158,12 +120,8 @@ const MainPage = () => {
               >
                 <div>
                   <div className="card-info">
-                    <p>
-                      <h2>Title: {eachProgram.program_title}</h2>
-                    </p>
-                    <p>
-                      <h3>Difficulty level: {eachProgram.program_level}</h3>
-                    </p>
+                    <h2>Title: {eachProgram.program_title}</h2>
+                    <h3>Difficulty level: {eachProgram.program_level}</h3>
                     <p>Type: {eachProgram.program_type}</p>
                     <p>{eachProgram.presentation}</p>
                   </div>
